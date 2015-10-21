a2ed88db94bc8aa10b351615ec5f451cc0454a7e
Carol Mirakove <carol@waybetter.com>
2015-10-21 14:02:14 -0400

    added test using dynamic var; updating tests per website changes

--- spec/global_spec.rb
+++ spec/global_spec.rb
@@ -2,9 +2,12 @@ require_relative 'spec_helper'
 require_relative '../pages/global'
 
 describe 'Global -' do
+  include HomeHelpers
 
-  let(:page) { Page.new(@driver) }
   let(:global) { Global.new(@driver) }
+  let(:home) { Home.new(@driver) }
+  let(:home_modal) { HomeModal.new(@driver) }
+  let(:page) { Page.new(@driver) }
 
   it 'load home page and verify global page elements', :smoke do
     page.load ''
@@ -15,8 +18,11 @@ describe 'Global -' do
 
   channels = [ 'facebook', 'twitter', 'tumblr', 'youtube', 'soundcloud' ]
   channels.each do |channel|
-    it 'click social link from header', :focus, :social do
-      page.load ''
+    it 'click social link from header', :social do
+      load_home_page_and_decline_modal # HomeHelpers
+      global.ui_elements do |ui_element|
+        expect(page.ui_element_displayed?(ui_element)).to be_truthy
+      end
       global.click_social_icon_from_header(channel)
       expect(page.title).to eq('poets.org | Academy of American Poets')
     end
