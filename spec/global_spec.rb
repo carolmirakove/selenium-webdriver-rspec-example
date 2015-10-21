45446aff55503bc130ea23427e81b61c41797a69
Carol Mirakove <carol@waybetter.com>
2015-10-21 17:56:55 -0400

    adding input-field regression testing

--- spec/global_spec.rb
+++ spec/global_spec.rb
@@ -1,5 +1,6 @@
 require_relative 'spec_helper'
 require_relative '../pages/global'
+require_relative '../pages/search'
 
 describe 'Global -' do
   include HomeHelpers
@@ -8,6 +9,7 @@ describe 'Global -' do
   let(:home) { Home.new(@driver) }
   let(:home_modal) { HomeModal.new(@driver) }
   let(:page) { Page.new(@driver) }
+  let(:search) { Search.new(@driver) }
 
   it 'load home page and verify global page elements', :smoke do
     page.load ''
@@ -26,5 +28,16 @@ describe 'Global -' do
       global.click_social_icon_from_header(channel)
       expect(page.title).to eq('poets.org | Academy of American Poets')
     end
+  end
+
+  it 'submit a search term that returns no results' do
+    keyword = "\"#{Faker::Lorem.words(rand(3..5)).join(' ')}\""
+
+    load_home_page_and_close_modal # HomeHelpers
+    global.submit_search_term(keyword)
+    search.ui_elements do |ui_element|
+      expect(page.ui_element_displayed?(ui_element)).to be_truthy
+    end
+    expect(search.no_results_displayed?).to be_truthy
   end
 end
